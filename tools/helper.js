e1cd136757afc530dab3ab5458f3f106b2e935bf
mohameddawood53 <mohamedahmeddawood40@gmail.com>
2024-09-21 22:49:39 +0300

    edit

--- tools/helper.js
+++ tools/helper.js
@@ -2,6 +2,7 @@ const readPkg = require("read-package-json");
 const figlet = require("figlet");
 const fs = require("fs/promises");
 const { fileURLToPath } = require('url')
+const path = require('path')
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename)
